90e548e0b897d87b73176a9abef416caa4ddc6a2
Michael Aversa <drivenbyhim@gmail.com>
2012-06-07 22:10:51 -0700

    Initial commit Lots of Asp.net fluff for a canvas demo :0

---
+++ ProgressBars/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProgressBars.Models;
+
+namespace ProgressBars.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index () {
+
+            var viewModel = new List<ProgressionData>();
+                
+            // Randomize data
+            var rnd = new Random();
+            for (int i = 0; i < 100; i++) {
+                var model = new ProgressionData() {
+                    Total = rnd.Next(1000, 10000),
+                    Progress = new List<int>()
+                };
+                
+                for (int j = 0; j < 5; j++) {
+                    model.Progress.Add(rnd.Next(0, model.Total + 1));
+                }
+
+                viewModel.Add(model);
+            }
+
+            return View(viewModel);
+        }
+
+        public ActionResult About () {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact () {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ProgressBars/Models/Progress.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProgressBars.Models {
+    public class ProgressionData {
+        public int Total { get; set; }
+        public List<int> Progress { get; set; }
+    }
+}
